f2604d048e6641669b849a71f22875d512f10e4e
Rickfalton <rickonyach@gmail.com>
2024-07-03 00:08:36 +0300

    object

---
+++ test/objects.js
@@ -0,0 +1,16 @@
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee };
+    newEmployee[key] = value;
+    return newEmployee;
+ }
+ 
+ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+ }
+ 
+ function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+ }
